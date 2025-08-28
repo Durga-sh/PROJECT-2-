d6db0829cc4df6169b2e5340b3b717bd1db4b362
Durgashankar Puhan <puhandurgasankarp@gmail.com>
2025-08-28 14:59:56 +0530

    Change Folder Structure

--- src/components/ui/input.jsx
+++
@@ -1,8 +0,0 @@
-export function Input({ className = "", ...props }) {
-    return (
-      <input
-        className={`border rounded-md px-3 py-2 focus:outline-none focus:ring-2 focus:ring-blue-500 ${className}`}
-        {...props}
-      />
-    );
-  }
